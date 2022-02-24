86171dafd1c39038e0f2f7d4941cc140ae787701
sixwizard <6112122@163.com>
2022-02-24 15:32:15 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/** @type {import('gatsby).GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: `my-site-name`,
+      siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+
+  ]
+}
