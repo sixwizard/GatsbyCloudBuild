e1bbb549e553acfe59aef7ca1662bd7c083f500b
sixwizard <6112122@163.com>
2022-03-16 11:11:46 +0800

    add gatsby image

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,9 @@ module.exports = {
       siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
+      `gatsby-plugin-image`,
+      `gatsby-plugin-sharp`,
+      `gatsby-transformer-sharp`,
       {
           resolve: `gatsby-source-graphql`,
           options: {
